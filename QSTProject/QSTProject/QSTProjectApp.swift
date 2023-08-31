74b70c803e67d240ed17b46cc04d8181ff15b2fd
Khoi Nguyen <khoinguyen@192.168.2.2>
2023-08-31 13:16:06 +0700

    Create QSTProject

---
+++ QSTProject/QSTProject/QSTProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  QSTProjectApp.swift
+//  QSTProject
+//
+//  Created by Khoi Nguyen on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct QSTProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
