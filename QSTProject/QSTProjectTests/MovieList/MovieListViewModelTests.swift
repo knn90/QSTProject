28bcb550f479a9d912b431e19f494542023ce204
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2023-08-31 14:25:23 +0700

    Init MovieListViewModelTests

---
+++ QSTProject/QSTProjectTests/MovieList/MovieListViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListViewModelTests.swift
+//  QSTProjectTests
+//
+//  Created by Khoi Nguyen on 31/08/2023.
+//
+
+import Foundation
+import XCTest
+
+final class MovieListViewModelTests: XCTestCase {
+}
+
