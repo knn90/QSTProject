042b82754a3ed3f1298a8a2d93dec014478bae3b
Khoi Nguyen <nguyenkhoi2190@gmail.com>
2023-08-31 13:51:57 +0700

    Create Movie model

---
+++ QSTProject/QSTProject/Models/Movie.swift
@@ -0,0 +1,22 @@
+//
+//  Movie.swift
+//  QSTProject
+//
+//  Created by Khoi Nguyen on 31/08/2023.
+//
+
+import Foundation
+
+struct Movie: Identifiable {
+    let title: String
+    let rating: CGFloat
+    let duration: String
+    let genre: String
+    let releasedDate: Date
+    let description: String
+    let trailer: URL
+
+    var id: String {
+        title
+    }
+}
--- QSTProject/QSTProject/MovieList/MovieListView.swift
+++ QSTProject/QSTProject/MovieList/MovieListView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct MovieListView: View {
     @ObservedObject var viewModel: MovieListViewModel
-    let data = Array(repeating: Movie(), count: 10)
     var body: some View {
-        List(data) { _ in
-            MovieCell()
-                .alignmentGuide(.listRowSeparatorLeading) { d in
-                    d[.leading] }
-        }
+        MovieCell()
+            .alignmentGuide(.listRowSeparatorLeading) { d in
+                d[.leading] }
     }
 }
 
@@ -23,8 +20,4 @@ struct MovieListView_Previews: PreviewProvider {
     static var previews: some View {
         MovieListView(viewModel: MovieListViewModel())
     }
-}
-
-struct Movie: Identifiable {
-    let id = UUID()
 }
